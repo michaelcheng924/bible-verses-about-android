12d3cd35e554059af74d5aa94a2a3957af5d2aa1
Michael Cheng <cheng.c.mike@gmail.com>
2024-01-15 14:24:54 -0600

    update

--- app/src/main/java/com/michaelcheng924/bibleversesabout/MainActivity.kt
+++ app/src/main/java/com/michaelcheng924/bibleversesabout/MainActivity.kt
@@ -17,11 +17,24 @@ import java.net.HttpURLConnection
 import java.net.URL
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.TextField
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -29,6 +42,11 @@ import androidx.compose.ui.unit.sp
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.core.text.HtmlCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -90,7 +108,7 @@ suspend fun fetchAndParseTopicJson(slug: String): Topic = withContext(Dispatcher
 
     val inputStream = connection.inputStream
     val jsonString = inputStream.bufferedReader().use { it.readText() }
-
+    Log.d("JsonString", jsonString)
 
     Json.decodeFromString<Topic>(jsonString)
 }
@@ -115,15 +133,15 @@ data class Topic(
 
 @Serializable
 data class Verse(
-    val _id: String,
-    val __v: Int,
-    val topicId: String,
-    val verse: String,
-    val esv: String,
-    val kjv: String,
-    val niv: String,
-    val votes: Int,
-    val modified: String
+    val _id: String? = "",
+    val __v: Int? = 0,
+    val topicId: String? = "",
+    val verse: String = "",
+    val esv: String = "",
+    val kjv: String = "",
+    val niv: String = "",
+    val votes: Int = 0,
+    val modified: String = ""
 )
 
 @Composable
@@ -193,20 +211,82 @@ fun ListScreen(navController: NavController) {
 @Composable
 fun DetailScreen(slug: String, navController: NavController) {
     var responseData by remember { mutableStateOf<Topic?>(null) }
+    var selectedTabIndex by remember { mutableStateOf(0) } // Default to KJV (0)
 
     LaunchedEffect(Unit) {
         responseData = fetchAndParseTopicJson(slug)
     }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "Bible Verses About $responseData.name",
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 30.sp,
-                color = MaterialTheme.colorScheme.onBackground
-            ),
-        )
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                IconButton(
+                    onClick = {
+                        // Navigate back to the ListScreen
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+
+            Text(
+                text = "${responseData?.verses?.size} Bible Verses About ${responseData?.name ?: ""}",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                ),
+            )
+
+            // TabRow for toggling between KJV and ESV
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
+                    )
+                },
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Tab(
+                    selected = selectedTabIndex == 0,
+                    onClick = { selectedTabIndex = 0 },
+                    text = { Text("KJV") }
+                )
+                Tab(
+                    selected = selectedTabIndex == 1,
+                    onClick = { selectedTabIndex = 1 },
+                    text = { Text("ESV") }
+                )
+            }
+        }
+        items(responseData?.verses ?: emptyList()) { verse ->
+            val verseText = if (selectedTabIndex == 0) verse.kjv else verse.esv
+            val htmlText = HtmlCompat.fromHtml(verseText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp)) {
+                        append("${verse.verse}\n")
+                    }
+                    val spanStyle = SpanStyle(fontSize = 18.sp)
+                    pushStyle(spanStyle)
+                    append(htmlText)
+                    pop()
+                },
+                style = TextStyle(fontSize = 18.sp),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
     }
 }
 
